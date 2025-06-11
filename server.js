96ad53707472b973c5c940da277c570a334dc7b8
GregoryRobetertson <gregoryrobertson.dev@gmail.com>
2025-06-11 09:27:26 -0500

    update

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
+require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
-const authRoutes = require("./src/server/routes/auth");
-const bookingRoutes = require("./src/server/routes/bookingRoutes");
+const authRoutes = require("./routes/auth");
+const bookingRoutes = require("./routes/bookingRoutes");
 const cors = require("cors");
 
-dotenv.config();
+console.log("FIREBASE_PRIVATE_KEY:", process.env.FIREBASE_PRIVATE_KEY);
 
 const app = express();
 
@@ -24,10 +25,14 @@ app.use(
 // Routes
 app.use("/api/auth", authRoutes);
 app.use("/api/bookings", bookingRoutes);
+
 // Mongoose connection
 mongoose
   .connect(process.env.MONGODB_URI)
   .then(() => {
-    app.listen(process.env.PORT || 5000, () => console.log("Server running"));
+    console.log("Connected to MongoDB");
+    app.listen(process.env.PORT || 5000, () =>
+      console.log(`Server running on port ${process.env.PORT || 5000}`)
+    );
   })
   .catch((err) => console.error("MongoDB connection error:", err));
