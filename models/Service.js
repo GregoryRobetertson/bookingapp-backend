3d3f4c5b825bc73df55d94bda3d496817446dd3f
Gregory Robertson <147750592+GregoryRobetertson@users.noreply.github.com>
2025-06-12 15:07:15 +0000

    add image url to service.js

--- models/Service.js
+++ models/Service.js
@@ -4,7 +4,8 @@ const serviceSchema = new mongoose.Schema({
   name: {type: string, required: true}
   type:  {type: string, required: true}
   description:  {type: string, required: true}
-  price: {type: number, required: true}
+  price: {type: number, required: true},
+  imageUrl: { type: String },
 });
 
 const Service = mongoose.model("service", serviceSchema);
