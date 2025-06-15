7d7ea76da535568911efefccc33ac20c8dfd39f5
GregoryRobetertson <gregoryrobertson.dev@gmail.com>
2025-06-14 20:03:18 -0500

    feat(schema): add workImage field to Service model as an array of image URLs

--- routes/serviceRoutes.js
+++ routes/serviceRoutes.js
@@ -6,29 +6,15 @@ const {
   getServices,
 } = require("../controllers/serviceController");
 
-router.post("/", createService);
 router.get("/", getServices);
 
-// ✅ THIS IS THE MISSING ROUTE
 router.post(
-  "/upload",
+  "/",
   upload.fields([
     { name: "image", maxCount: 1 },
-    { name: "workimage", maxCount: 1 },
+    { name: "workImage", maxCount: 1 },
   ]),
-  (req, res) => {
-    const image = req.files["image"]?.[0];
-    const workImage = req.files["workImage"]?.[0];
-
-    console.log("Image uploaded:", image?.path);
-    console.log("Work image uploaded:", workImage?.path);
-
-    res.json({
-      message: "Upload successful",
-      imageUrl: image?.path,
-      workImageUrl: workImage?.path,
-    });
-  }
+  createService
 );
 
 module.exports = router;
